400145d69420e0cf89002f01de8ee5f12334f2a5
Chibuzor Obiora <nbyodai@gmail.com>
2025-11-30 11:23:09 -0500

    add command line function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello, World!");
+import { argv } from 'node:process';
+
+function main() {
+    if (argv.length < 3) {
+        console.log('too few arguments')
+        process.exit(1);
+    }
+    if (argv.length > 3){
+        console.log('too many arguments');
+        process.exit(1);
+    }
+    const baseURL = argv[2];
+
+    console.log(`Now crawling "${baseURL}" ...`);
+    process.exit(0);
+}
+
+main();
